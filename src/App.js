e39f5921def0e4fb93cef7fed1c845c2768bcdf1
ellenwi <ellenwi@student.chalmers.se>
2020-04-16 21:39:13 +0200

    Created the start page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="header">
+        CityPop
+      </h1>
+      <div className="center">
+        <button className="button-category">
+          SEARCH BY CITY
+        </button>
+        <button className="button-category">
+          SEARCH BY COUNTRY
+        </button>
+      </div>
     </div>
   );
 }
