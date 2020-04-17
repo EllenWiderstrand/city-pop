f3dab993460bebf99a822a2f25e6aa4ae6df0524
ellenwi <ellenwi@student.chalmers.se>
2020-04-17 14:32:53 +0200

    Made it possible to change between pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import StartPage from './StartPage'
-import Search from './Search'
+import Search from './SearchCity'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 class App extends React.Component {
   render(){
     return (
       <div>
+        {/* The header CityPop is always portrayed */}
         <Header/>
-        <Search/>
+        <Router>
+          {/* Renders the class whose path matches the current URL */}
+          <Switch>
+            <Route exact path="/">
+              <StartPage />
+            </Route>
+            <Route exact path="/search-city">
+              <Search />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
     }
--- src/StartPage.js
+++ src/StartPage.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
+import { Redirect } from "react-router-dom";
 
 class StartPage extends React.Component {
+    // Sets the initial state
+    state = {
+        redirect: false,
+        city: false
+    }
+    // Updated the states when it is going to redirect to 'search-city'
+    setCityRedirect = () => {
+        this.setState({
+            redirect: true,
+            city:true
+        })
+    }
+    // Redirects to either 'search-city' or 'search-country'
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            if (this.state.city){
+                return <Redirect to='/search-city' />
+            }
+        }
+    }
+
     render(){
         return (
             <div className="center">
-                <button className="button-category">
+                {this.renderRedirect()}
+                <button onClick={this.setCityRedirect} className="button-category">
                     SEARCH BY CITY
                 </button>
                 <button className="button-category">
